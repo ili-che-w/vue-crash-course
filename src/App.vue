d8db2dea96a01ac78727580262bc7e32b1d2e467
Ильичёв Иван Сергеевич <iilichev@corp.local>
2021-08-20 16:07:03 +0300

    [v+ | +] vue-router, routes

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,9 @@
 <template>
   <div id="app">
     <h1>Todo application</h1>
-    <AddTodo @add-todo-item="addTodoItem" />
-    <hr />
-    <TodoList :todos="todos" @remove-todo-item="removeTodoItem" />
+    <router-view />
   </div>
 </template>
-
-<script>
-import TodoList from '@/components/TodoList'
-import AddTodo from '@/components/AddTodo'
-
-export default {
-  name: 'App',
-  data() {
-    return {
-      todos: []
-    }
-  },
-  components: {
-    TodoList,
-    AddTodo
-  },
-  mounted() {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(response => response.json())
-      .then(json => {
-        this.todos = json
-      })
-  },
-  methods: {
-    removeTodoItem(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    },
-    addTodoItem(todo) {
-      this.todos.push(todo)
-    }
-  }
-}
-</script>
 
 <style>
 #app {
---
+++ src/views/Todos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Todo list</h2>
+    <AddTodo @add-todo-item="addTodoItem" />
+    <hr />
+    <TodoList :todos="todos" @remove-todo-item="removeTodoItem" />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: []
+    }
+  },
+  components: {
+    TodoList,
+    AddTodo
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
+  },
+  methods: {
+    removeTodoItem(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodoItem(todo) {
+      this.todos.push(todo)
+    }
+  }
+}
+</script>
