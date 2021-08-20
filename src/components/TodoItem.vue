34fe53e3be50d456d61fc296ae2cca3981692e5e
Ильичёв Иван Сергеевич <iilichev@corp.local>
2021-08-20 15:21:29 +0300

    [*] todo list & items styles

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,12 @@
 <template>
   <li>
-    {{ todo.title }}
+    <span>
+      <input type="checkbox" class="todo__checkbox" />
+      <strong>{{ todo.id }}</strong>
+      {{ todo.title }}
+    </span>
+
+    <button class="todo__remove">&times;</button>
   </li>
 </template>
 
@@ -14,3 +20,32 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+li {
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 2rem;
+  margin-bottom: 1rem;
+}
+
+.todo__remove {
+  background-color: rgb(206, 23, 23);
+  color: white;
+  border: none;
+  border-radius: 50%;
+  font-size: 20px;
+  line-height: 25px;
+  height: 25px;
+  width: 25px;
+}
+
+.todo__checkbox {
+  margin-right: 1rem;
+}
+
+.todo--completed {
+  text-decoration: line-through;
+}
+</style>
