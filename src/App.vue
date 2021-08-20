71e7f3b0645db9eadaaf3bd31692fc4b7e4515a6
Ильичёв Иван Сергеевич <iilichev@corp.local>
2021-08-20 15:57:13 +0300

    [+] get todos from api

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,19 @@ export default {
   name: 'App',
   data() {
     return {
-      todos: [
-        { id: 1, title: 'Buy bread', completed: false },
-        { id: 2, title: 'Buy butter', completed: false },
-        { id: 3, title: 'Buy beer', completed: false }
-      ]
+      todos: []
     }
   },
   components: {
     TodoList,
     AddTodo
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
   },
   methods: {
     removeTodoItem(id) {
